c44b6f8f1799fb96e1b4cf5e4a59d79ca338eab3
Thallys Freitas Soares <thallys@hotmail.com>
2021-11-09 17:38:14 -0300

    operações da calculadora

--- 01_RestWithASPNETUdemy_Scaffold/RestWithASP.NET5Udemy/RestWithASP.NET5Udemy/Startup.cs
+++ 01_RestWithASPNETUdemy_Scaffold/RestWithASP.NET5Udemy/RestWithASP.NET5Udemy/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,7 +25,8 @@ namespace RestWithASP.NET5Udemy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,15 +36,8 @@ namespace RestWithASP.NET5Udemy
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -49,7 +45,7 @@ namespace RestWithASP.NET5Udemy
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
