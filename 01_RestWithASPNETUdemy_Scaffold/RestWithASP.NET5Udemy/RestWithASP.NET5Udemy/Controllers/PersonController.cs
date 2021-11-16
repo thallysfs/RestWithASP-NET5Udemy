a041d3395cf0636d0ee2604e9270fa4e862b0d7b
Thallys Freitas Soares <thallys@hotmail.com>
2021-11-16 09:52:13 -0300

    Criação de model

---
+++ 01_RestWithASPNETUdemy_Scaffold/RestWithASP.NET5Udemy/RestWithASP.NET5Udemy/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP.NET5Udemy.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+
+            return BadRequest("Invalid Input");
+        }
+
+
+
+
+    }
+}
